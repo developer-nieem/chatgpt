9b7186d5a528f7a02e54c648cef1c0eceacf7434
developer-nieem <naimhossenwork@gmail.com>
2023-10-23 12:23:37 +0600

    setup prohect

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./App.css";
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+      {/* sidebar */}
+      <div>
+          {/* sidebar top */}
+              <div>
+                <img src="" alt="" />
+                <button> + Add new Chat</button>
+
+                <div>
+                  <h4>Previous Chat</h4>
+                  <button>What is css</button>
+                </div>
+              </div>
+          {/* sidebar bottom */}
+          <div>
+
+          </div>
+      </div>
+
+      {/* Main content */}
+      <div>
+        <div>
+
+        </div>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
